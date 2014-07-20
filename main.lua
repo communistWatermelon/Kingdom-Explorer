add7724fc9505b02db7f20576c070e83e341d705
Jacob Miner <zyko101@gmail.com>
2014-07-20 01:43:01 -0700

    Added proper gesture code for the game.

--- main.lua
+++ main.lua
@@ -28,6 +28,7 @@ function love.load()
    	anim = newAnimation(img, 96, 96, 0.1, 0)
    	anim:setMode("once")
    	anim:stop()
+   	dragging = { active = false, diffX = 0, diffY = 0 }
 end
 
 function love.touchpressed(id, x, y, pressure)
@@ -38,15 +39,19 @@ function love.touchpressed(id, x, y, pressure)
 		controllerPressed(tempx, tempy)	
 	else
 		anim:play()
+		startSwipe(tempx, tempy) 
 	end
-
 end
 
 function love.touchreleased(id, x, y, pressure)
+	local tempx, tempy
+	tempx = x * lg.getWidth()
+	tempy = y * lg.getHeight()
 	if (x <= .5) then
 		controllerReleased()
 	else
 		anim:reset()
+		endSwipe(tempx, tempy)
 	end
 	
 end
@@ -65,24 +70,78 @@ function love.update(dt)
 	anim:update(dt)   
 end
 
-function love.mousepressed(x, y, button)
-	if button == "l" then
-		if (x < lg.getWidth()/2) then
-			controllerPressed(x, y)	
-		else
-			anim:play()
+function love.mousepressed(x, y, button, isTouch)
+	if not isTouch then
+		if button == "l" then
+			if (x < lg.getWidth()/2) then
+				controllerPressed(x, y)	
+			else
+				anim:play()
+			end
+			startSwipe(x, y) 
 		end
-	end
 
-	if button == "r" then
-		print(lm.getPosition())
+		if button == "r" then
+			print(lm.getPosition())
+		end
 	end
 end
 
-function love.mousereleased(x, y, button)
-	if button == "l" then 
-		controllerReleased() 
-		anim:reset()
+function startSwipe(x, y)
+	dragging.active = True
+	dragging.diffX = x
+	dragging.diffY = y
+end
+
+function endSwipe(x, y)
+	dragging.active = false
+	dragging.diffX = x - dragging.diffX
+	dragging.diffY = y - dragging.diffY
+	local swipe = swipeDirection()
+	
+	if swipe == "down" then
+		-- show inventory screen
+		drawInventory()
+	elseif swipe == "up" then
+		 -- show map screen
+	elseif swipe == "left" then
+		--switch to item to the left
+	elseif swipe == "right" then
+		--switch to item to the right
+	end
+end
+
+function swipeDirection()
+	local swipeHeight = lg.getHeight()
+	local swipeWidth = lg.getWidth()
+	local lowSwipe = 2
+	local highSwipe = 4
+	local direction
+
+	if (math.abs(dragging.diffY) > math.abs(dragging.diffX)) then
+		if dragging.diffY > (swipeHeight / highSwipe) then
+			direction = "down"
+		elseif dragging.diffY < -(swipeHeight / highSwipe) then
+			direction = "up"
+		end
+	elseif (math.abs(dragging.diffX) > math.abs(dragging.diffY)) then
+		if dragging.diffX < -(swipeWidth / highSwipe) then
+			direction = "left"
+		elseif dragging.diffX > (swipeWidth / highSwipe) then
+			direction = "right"
+		end
+	end
+	
+	return direction
+end
+
+function love.mousereleased(x, y, button, isTouch)
+	if not isTouch then
+		if button == "l" then 
+			controllerReleased() 
+			anim:reset()
+			endSwipe(x, y)
+		end
 	end
 end
 
