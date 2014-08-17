15d78b2c2f54c2084d8592908732a9ea720a9152
Jacob Miner <zyko101@gmail.com>
2014-08-16 19:28:39 -0700

    Moved attack collision to character file, should simplify code.

--- characters/characters.lua
+++ characters/characters.lua
@@ -43,9 +43,50 @@ function checkCollision(param, param2)
 	size2 = getSize(param2)
 
 	return loc1.x < loc2.x + size2.width and
-				loc2.x < loc1.x + size1.width and
-				loc1.y < loc2.y + size2.height and
-				loc2.y < loc1.y + size1.height
+			loc2.x < loc1.x + size1.width and
+			loc1.y < loc2.y + size2.height and
+			loc2.y < loc1.y + size1.height
+end
+
+function attackCollision(item, x, y, width, height, facing)
+	local attackx, attacky
+	local attackw, attackh
+
+	for j=1, #item.target do
+		for i=1, #item.target[j] do
+			local tarSize = getSize(item.target[j][i])
+			local tarLoc = getLocation(item.target[j][i])
+			
+			if facing == 0 then
+				attackx = x
+				attacky = y - (width + (getWidth(hero)/2))
+				attackw = width
+				attackh = height 
+			elseif facing < 0 then
+				attackx = x - (width + (getWidth(hero)/2))
+				attacky = y
+				attackw = height
+				attackh = width 
+			elseif facing > 0 and facing < 3 then
+				attackx = x + (width + (getWidth(hero)/2))
+				attacky = y
+				attackw = height
+				attackh = width 
+			elseif facing > 3 then
+				attackx = x
+				attacky = y + (width + (getWidth(hero)/2))
+				attackw = width
+				attackh = height 
+			end
+
+			if tarLoc.x < attackx + attackw and
+				attackx < tarLoc.x + tarSize.width and
+				tarLoc.y < attacky + attackh and
+				attacky < tarLoc.y + tarSize.height then
+					changeHealth(item.target[j][i], -getAttack(item))
+			end
+		end
+	end
 end
 
 function checkCollisionsTable(param, table)
--- items/lv1sword.lua
+++ items/lv1sword.lua
@@ -1,62 +1,22 @@
 local path = "items/lv1sword/"
 -- each item should have its own lua file in the items folder
 function loadLv1Sword()
-	sword = { atk = 50, sprite = lg.newImage(path .. "sword.png"), }
 	local img = love.graphics.newImage(path .. "swordanim.png")
    	local anim = newAnimation(img, 128, 128, 0.025, 9)
    	anim:setMode("once")
    	anim:stop()
 
-
-   	sword = { stats = { atk = 50 },
-			draw = { sprite = lg.newImage(path .. "sword.png"), attack = anim },
-			size = { width = 32, height = 32}
-		}
+   	sword = { 	stats = { atk = 50 },
+				draw = { sprite = lg.newImage(path .. "sword.png"), attack = anim },
+				size = { width = 32, height = 32},
+				target = { mobTable }
+			}
 end
 
 function useLv1Sword(x, y)
-	Lv1SwordCollision(x, y)
+	attackCollision(sword, x, y, getWidth(sword), getHeight(sword), getFacing(hero))
 	getAnim(sword, "attack"):play()
 	getAnim(sword, "attack"):reset()
-end
-
-function Lv1SwordCollision(x, y)
-	local facing = getFacing(hero)
-	local swordx, swordy
-	local swordw, swordh
-
-	for i=1, #mobTable do
-		local mobSize = getSize(mobTable[i])
-		local mobLoc = getLocation(mobTable[i])
-		if facing == 0 then
-			swordx = x
-			swordy = y - 48
-			swordw = 48
-			swordh = 32 
-		elseif facing < 0 then
-			swordx = x - 48
-			swordy = y
-			swordw = 32
-			swordh = 48 
-		elseif facing > 0 and facing < 3 then
-			swordx = x + 48
-			swordy = y
-			swordw = 32
-			swordh = 48 
-		elseif facing > 3 then
-			swordx = x
-			swordy = y + 48
-			swordw = 48
-			swordh = 32 
-		end
-
-			if mobLoc.x < swordx + swordw and
-			swordx < mobLoc.x + mobSize.width and
-			mobLoc.y < swordy + swordh and
-			swordy < mobLoc.y + mobSize.height then
-				changeHealth(mobTable[i], -getAttack(sword))
-		end
-	end
 end
 
 function animateLv1Sword(x, y)
